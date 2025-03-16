8265cc5c7c8b56029177da12f814e06b510f400e
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-03-16 21:46:01 +0800

    Update Button.tsx

--- frontend/src/components/AddProductModal/Button.tsx
+++ frontend/src/components/AddProductModal/Button.tsx
@@ -13,8 +13,8 @@ export const Button = ({
 }: ButtonProps) => {
   const baseStyles = "px-6 py-2 rounded-[5px]";
   const variantStyles = {
-    primary: "bg-[#1B626E] text-white",
-    secondary: "border-[1px] border-[#1B626E] text-[#1B626E]",
+    primary: "bg-[#1B626E] text-white transition-colors hover:bg-[#297885] active:bg-[#145965]",
+    secondary: "border-[1px] border-[#1B626E] text-[#1B626E] transition-colors hover:bg-[#1B626E]/5 active:bg-[#1B626E]/10",
   };
   return (
     <button
