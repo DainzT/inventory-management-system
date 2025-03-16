e470654b8e6d859941a199a027c0449a5f5dc07a
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-03-16 22:05:37 +0800

    Create & Update AddProductModal.tsx, AddProductForm.tsx, InputField.tsx, PriceInput.tsx, QuantityInput.tsx, UnitSelector.tsx
    feat: AddProductForm.tsx that submits productdata
    feat: InputField.tsx for product note and name
    feat: PriceInput.tsx for inputting product price
    feat: QuantityInput.tsx for inputting stock quantity
    feat: UnitSelector.tsx for selecting unit type

---
+++ frontend/src/components/AddProductModal/AddProductForm.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
+import { InputField } from "./InputField";
+import { PriceInput } from "./PriceInput";
+import { QuantityInput } from "./QuantityInput";
+import { UnitSelector } from "./UnitSelector";
+
+interface ProductFormData {
+    productName: string;
+    note: string;
+    quantity: number | "";
+    unitPrice: number | "";
+    selectUnit: string;
+    unitSize: number | "";
+    total: number | "";
+  }
+
+interface ProductFormProps {
+    onCancel: () => void;
+    onSubmit: (data: ProductFormData) => void;
+  }
+
+const AddProductForm = ({ 
+    onCancel, 
+    onSubmit,
+}: ProductFormProps) => {
+    const [productData, setProductData] = useState<ProductFormData>({
+        productName: "",
+        note: "",
+        quantity: "",
+        selectUnit: "",
+        unitPrice: "",
+        unitSize: "",
+        total: "",
+    }); 
+
+    useEffect(() => {
+        setProductData((current) => ({
+            ...current,
+            total: Number(current.unitPrice) * (Number(current.quantity) / Number(current.unitSize)),
+        }));
+    }, [productData.quantity, productData.unitPrice,  productData.unitSize]);
+    
+    const handleInputChange = (field: keyof ProductFormData, value: string | number) => {
+        setProductData((prevData) => ({
+          ...prevData,
+          [field]: value,
+        }));
+    };
+    
+    const handleFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        onSubmit(productData); 
+    };
+
+    return (
+        
+        <form onSubmit={handleFormSubmit}  className="flex flex-col gap-6">
+            <InputField
+                label="Product Name"
+                required
+                value={productData.productName}
+                onChange={(value) => handleInputChange("productName", value)}
+                placeholder={"Enter product name"}
+            />
+            <InputField
+                label="Note"
+                required
+                type="textarea"
+                value={productData.note}
+                onChange={(value) => handleInputChange("note", value)}
+                placeholder={"Enter note"}
+            />
+            <div className="flex gap-6">
+                <QuantityInput
+                    required
+                    value={productData.quantity}
+                    onChange={(value) => handleInputChange("quantity", value)} 
+                />
+
+                <UnitSelector
+                    value={productData.selectUnit}
+                    onChange={(value) => handleInputChange("selectUnit", value)} 
+                />
+            </div>
+            <PriceInput
+                label="Price per Unit"
+                required
+                value={productData.unitPrice}
+                unit={productData.selectUnit}
+                unitSize={productData.unitSize}
+                quantity={productData.quantity}
+                unitChange={(value)  => handleInputChange("unitSize", value)}
+                onChange={(value) => handleInputChange("unitPrice", value)}
+            />
+            <PriceInput 
+                label="Total" 
+                value={productData.total} 
+                readonly 
+            />
+            
+            <div className="flex justify-end gap-4 mt-6">
+                <Button 
+                    variant="secondary" 
+                    onClick={onCancel}
+                >
+                        Cancel
+                </Button>
+                <Button 
+                    type="submit"
+                >
+                        Add Product
+                </Button>
+            </div>
+        </form>
+    );
+};
+
+export default AddProductForm;
---
+++ frontend/src/components/AddProductModal/PriceInput.tsx
@@ -0,0 +1,99 @@
+
+interface PriceInputProps {
+  label: string;
+  value: number | "";
+  unitSize?: number | "";
+  onChange?: (value: number | "") => void;
+  unitChange?: (value: number | "") => void;
+  readonly?: boolean;
+  required?: boolean;
+  unit?: string;
+  quantity?: number | "";
+}
+
+export const PriceInput = ({
+  label,
+  value,
+  unitSize,
+  onChange,
+  unitChange,
+  readonly = false,
+  required = false,
+  unit,
+  quantity,
+}: PriceInputProps) => {
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    if (inputValue === "") {
+      onChange?.("");
+    } else {
+      const parsedValue = parseFloat(inputValue);
+      if (!isNaN(parsedValue)) {
+        onChange?.(parsedValue);
+      }
+    }
+  };
+
+  const handleUnitChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    if (inputValue === "") {
+      unitChange?.("");
+    } else {
+      const parsedValue = parseFloat(inputValue);
+      if (!isNaN(parsedValue)) {
+        unitChange?.(parsedValue);
+      }
+    }
+  };
+  
+  function roundTo(num: number, precision: number): number {
+    const factor = Math.pow(10, precision);
+    return Math.round(num * factor) / factor;
+  }
+
+  return (
+    <div>
+        <label className="text-[16px] font-bold inter-font">
+          <span>{label} </span>
+          {!readonly && <span className="text-[#FF5757]">*</span>}
+        </label>
+        <div className="relative mt-2">
+          <span className="absolute left-4 top-[8px] text-[#666] inter-font">₱</span>
+          <input
+            type="number"
+            value={value !== "" ? roundTo(value, 2) : ""}
+            onChange={handleChange}
+            placeholder="0.00"
+            min="0"
+            step="0.01"
+            readOnly={readonly}
+            className={`w-[140px] h-[40px] pl-8 rounded-[8px] border-[1px] inter-font ${
+              readonly
+                ? "border-[#F8F8F8] bg-[rgba(244,241,241,0.77)] text-[#666] cursor-default pointer-events-none"
+                : "border-[#0FE3FF] bg-[#F4F1F1]"
+            }`}
+            required={required}
+          />
+          {required && (
+            <>
+              <span className=" ml-2 mr-2 inter-font">per</span>
+              <input
+                type="number"
+                value={typeof unitSize === "number" ? roundTo(unitSize, 2) : ""}
+                onChange={handleUnitChange}
+                placeholder="0.00"
+                max={typeof quantity === "number" ? quantity : undefined}
+                min="0"
+                step="0.01"
+                readOnly={readonly}
+                className={`w-[70px] h-[40px] pl-3 rounded-[8px] border-[1px] inter-font border-[#0FE3FF] bg-[#F4F1F1]`}
+                required={required}
+              />
+              <span className="ml-2 inter-font">{unit?.trim() || "unit"}</span>
+            </>
+          )}
+        </div>
+    </div>
+  );
+};
