9e93608ab96dff91e6552e3d39252e265b5d966c
kailouss <kaizensomosera.04@gmail.com>
2025-04-30 03:11:50 +0800

    UPDATE jest.config and jest.setup

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,18 +1,24 @@
-import type { Config } from "@jest/types";
+import { defaultsESM } from "ts-jest/presets";
 
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  ...defaultsESM,
   testEnvironment: "jsdom",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testMatch: [
     "**/__tests__/**/*.test.[jt]s?(x)",
     "**/?(*.)+(spec|test).[jt]s?(x)",
   ],
   testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   watchPathIgnorePatterns: ["node_modules", "dist"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -2,3 +2,10 @@ import "@testing-library/jest-dom";
 const { TextEncoder, TextDecoder } = require("text-encoding");
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+Object.defineProperty(global, "import.meta", {
+  value: {
+    env: {
+      VITE_API_URL: "http://localhost:3000",
+    },
+  },
+});
