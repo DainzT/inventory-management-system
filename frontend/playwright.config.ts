b916e9b8e433de8e0fb30cbd9a8b004089bb380b
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-05-07 06:12:45 +0800

    Update playwright yml & config, package.json,
    Force use localhost

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -6,7 +6,6 @@ dotenv.config();
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
