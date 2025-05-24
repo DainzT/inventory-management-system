701538f7a46a110601936741f411ba1b4abc8069
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-05-24 23:06:54 +0800

    Update Invoice.stories.tsx

--- frontend/src/stories/Summary/Invoice.stories.tsx
+++ frontend/src/stories/Summary/Invoice.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryFn } from "@storybook/react";
-import { Invoice, InvoiceProps } from "@/components/Sum/Invoice";
+import { Invoice, InvoiceProps } from "@/components/summary/Invoice";
 import { OrderItem } from "@/types/order-item";
 
 
